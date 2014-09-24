c32ceff80f7cff415e8b00c6d0f5f0effaf4cdf9
wyfmor <wyfmor@163.com>
2014-09-25 00:26:56 +0800

    add shadow

--- gen/com/bluemor/reddotface/R.java
+++ gen/com/bluemor/reddotface/R.java
@@ -26,6 +26,7 @@ public final class R {
         public static final int default_face=0x7f020002;
         public static final int ic_launcher=0x7f020003;
         public static final int reddot=0x7f020004;
+        public static final int shadow=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f090010;
